0ccf371ee4112b71a65b26ed96925887c08975b8
rstyczynski <rstyczynski@gmail.com>
2024-06-18 16:26:17 +0200

    .

--- _sandbox/resource.tf
+++
@@ -1,9 +0,0 @@
-data "local_file" hello {
-  for_each = toset(["/Users/rstyczynski/hello.txt"])
-
-  filename = each.value
-}
-
-output "name" {
-  value = data.local_file.hello["/Users/rstyczynski/hello.txt"].content
-}
